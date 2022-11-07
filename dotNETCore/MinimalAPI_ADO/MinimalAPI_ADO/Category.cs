2e2536925c848eb2d737e1d0eaa0b176856c51aa
ajaysingala <ajay.singala@gmail.com>
2022-11-07 12:45:28 -0500

    minimal api with ado

---
+++ dotNETCore/MinimalAPI_ADO/MinimalAPI_ADO/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace MinimalAPI_ADO
+{
+    public class Category
+    {
+        public int Categoryid { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+    }
+}
