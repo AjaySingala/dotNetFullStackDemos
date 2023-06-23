5e310d7429496404411781ea0fe2702af22497a0
AjaySingala <ajay.singala@gmail.com>
2023-06-23 15:18:32 +0530

    yield demo

---
+++ GeneralDemos/YieldDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YieldDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            YieldDemo();
+        }
+
+        static void YieldDemo()
+        {
+            //var nums1 = GetNumberArray();
+            var nums2 = GetNumberArrayWithYield();
+            foreach(var n in nums2)
+            {
+                Console.WriteLine(n);
+            }
+        }
+
+        static int[] GetNumberArray()
+        {
+            int[] nums = new int[10];
+            for(int i = 0; i < 10; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    // ???
+                }
+            }
+            return nums;
+        }
+
+        //static IEnumerable<int> GetNumberArrayWithYield()
+        static IEnumerable<int> GetNumberArrayWithYield()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return i;
+                }
+            }
+        }
+
+    }
+}
